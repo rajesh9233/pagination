093a0953cb35865c3a0e08b5b62f401b4e2b0ddb
Rajesh <rajeshmurugesan228@gmail.com>
2021-07-01 09:12:39 +0530

    latest changes

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ fetch('https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenatio
             details+= `<td>${value.email}</td></tr>`
         }
     });
+    // var pagination = document.getElementById('pagination');
+    // const pages = personalInfo.length/10;
+    // for (let index = 0; index < pages; index++) {
+    //     var li = document.createElement('li');
+    //     li.setAttribute('class','page-item');
+    //     var a = document.createElement('a');
+    //     a.setAttribute('class','page-link');
+    //     a.setAttribute('href','#');
+    //     a.innerHTML = index+1;
+    //     li.append(a);
+    //     pagination.append(li);
+    // }
     row.innerHTML = details;
 });
 
@@ -28,50 +40,68 @@ function pageChange(id,start,end){
     var page3 = document.getElementById('page3');
     var next = document.getElementById('next');
     var last = document.getElementById('last');
-    first.classList.remove('active');
-    prev.classList.remove('active');
+    // first.classList.remove('active');
+    // prev.classList.remove('active');
     page1.classList.remove('active');
     page2.classList.remove('active');
     page3.classList.remove('active');
-    next.classList.remove('active');
-    last.classList.remove('active');
-    currentItem.classList.add('active')
+    // next.classList.remove('active');
+    // last.classList.remove('active');
+    first.classList.remove('disabled');
+    prev.classList.remove('disabled');
+    next.classList.remove('disabled');
+    last.classList.remove('disabled');
 
     console.log(id)
     switch (id) {
         case 'first':
             pageStart = 0;
             pageEnd = 10;
+            prev.classList.add('disabled');
             break;
         
         case 'prev':
-            pageStart = start-10;
-            pageEnd = end-10;
+            if(start !== 0 && end !== 10){
+                pageStart = start-10;
+                pageEnd = end-10;
+            }else {
+                first.classList.add('disabled');
+                prev.classList.add('disabled');
+            }
             break;
         
         case 'page1':
             pageStart=0;
             pageEnd=10;
+            currentItem.classList.add('active')
             break;
         
         case 'page2':
             pageStart=10;
             pageEnd=20;
+            currentItem.classList.add('active')
             break;
         
         case 'page3':            
             pageStart=20;
             pageEnd=30;
+            currentItem.classList.add('active')
             break;
         
         case 'next':
-            pageStart = start+10;
-            pageEnd = end+10;
+            if(start !== personalInfo.length - 10 && end !== personalInfo.length){
+                pageStart = start+10;
+                pageEnd = end+10;
+            }else {
+                next.classList.add('disabled');
+                last.classList.add('disabled');
+            }
             break;
         
         case 'last':
             pageStart = personalInfo.length - 10;
             pageEnd = personalInfo.length;
+            next.classList.add('disabled');
             break;
         
         default:
